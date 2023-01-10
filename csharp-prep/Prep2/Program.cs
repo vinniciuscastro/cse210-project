dcf831428ae856f2ed89a51fbe57638a4cc0e272
sibervi <vinnicius.castro97@gmail.com>
2023-01-10 14:53:31 -0700

    done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade? ");
+        string prompt = Console.ReadLine();
+
+        string valueInText = prompt;
+        int grade = int.Parse(valueInText);
+
+        Console.WriteLine("");
+
+        if (grade > 89) {
+            Console.WriteLine("You got an A congrats");
+        }
+
+        else if (grade > 79) {
+            Console.WriteLine("You got a B great job");
+        }
+
+        else if (grade > 69) {
+            Console.WriteLine("You got a C");
+        }
+
+        else if (grade > 59) {
+            Console.WriteLine("You got a D you failed the class");
+        }
+        else if (grade < 60) {
+            Console.WriteLine("You got a F you failed the class");
+        }
+
+        
+
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is Vinni");
+        Console.WriteLine(AddNUmbers(2,2));
+    }
+
+    static int AddNUmbers(int num1, int num2) {
+        return num1 + num2;
     }
 }
