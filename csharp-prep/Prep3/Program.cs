b22a8542faa5edf88a044b9a98992eb14c5ec2af
sibervi <vinnicius.castro97@gmail.com>
2023-01-12 14:58:55 -0700

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random generator = new Random();
+        int magicNumber = generator.Next(0, 10);
+        Console.WriteLine($"What is the magic number? {magicNumber}");
+        
+        bool keepPlaying = true;
+
+        while(keepPlaying) {
+            Console.Write("What is your guess? ");
+            int prompt = int.Parse(Console.ReadLine() ??"");
+
+            if (magicNumber == prompt) {
+                Console.WriteLine("Nice! You guessed it!");
+                keepPlaying = false; 
+            } else if (magicNumber > prompt) {
+                Console.WriteLine("Higher");
+           } else {
+                 Console.WriteLine("Lower");
+           }
+               
+           }
+
+           
+
+
+
     }
 }
