cd7990ccf213bb1d1082efb234503ee57fc6bef4
sibervi <vinnicius.castro97@gmail.com>
2023-02-24 13:18:52 -0700

    DONE <3

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -39,8 +39,79 @@ class Program
                     Console.WriteLine("");
                     current = DateTime.Now;
                 }
-                activity1.displayEnd(input);
+                activity1.displayEnd(input, "Breathing");
+            }
+
+            else if (prompt == 2) {
+                Console.Clear();
+                Reflection activity2 = new Reflection("Well Done!!","This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.",1);
+                Console.WriteLine($"{activity2.displaySummary()}");
+                Console.WriteLine("");
+                Console.Write("How long, in seconds, would you like for your session? "); 
+                int input = int.Parse(Console.ReadLine() ??"");
+                Console.Clear();
+                Console.WriteLine("Get Ready...");
+                activity2.GetReady();
+                Console.WriteLine("");
+                activity2.FirstPrompt();
+                Console.Clear();
+                DateTime startTime = DateTime.Now;
+                DateTime futureTime = startTime.AddSeconds(input);     
+
+                DateTime current = DateTime.Now;
+                while (current < futureTime) 
+                {
+                    activity2.DisplayQuestion();
+                    Console.Write("\n");
+                    current = DateTime.Now;
+                }
                 
+                activity2.displayEnd(input, "Reflection");
+
+            }
+            else if (prompt == 3) {
+                Console.Clear();
+                Listening activity3 = new Listening("Well Done!!","This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.",1);
+                Console.WriteLine($"{activity3.displaySummary()}");
+                Console.WriteLine("");
+                Console.Write("How long, in seconds, would you like for your session? "); 
+                int input = int.Parse(Console.ReadLine() ??"");
+                Console.Clear();
+                Console.WriteLine("Get Ready...");
+                activity3.GetReady();
+                Console.WriteLine("");
+                activity3.Prompts();
+                DateTime startTime = DateTime.Now;
+                DateTime futureTime = startTime.AddSeconds(input);     
+
+                DateTime current = DateTime.Now;
+                Console.WriteLine($"You may begin in: ");
+                while (current < futureTime) 
+                {
+                    activity3.userEntries();
+                    current = DateTime.Now;
+                }
+
+                activity3.AllEntries();
+                
+                activity3.displayEnd(input, "Listening");
+            }
+            // Creativity: I decide to create an error message so if the user type something that it is not on the menu they can 
+            else {
+                Console.Clear();
+                Reflection error = new Reflection("--","--",1);
+                Console.WriteLine("______________________________________");
+                Console.WriteLine("Error: Input not valid!");
+                error.GetReady();
+                Console.WriteLine("");
+                Console.Write($"Please try again in: ");
+                for (int i = 6; i > 0; i-- ) {
+                    
+                    Console.Write(i);
+                    Thread.Sleep(1000);
+                    Console.Write("\b \b");
+                }
+
             }
 
         }
@@ -61,25 +132,6 @@ class Activity {
         _duration = duration;
         
     }
-
-    // public string GetName(){
-    //     return _name;
-    // }
-    // public void SetName(string name) {
-    //     _name = name;
-    // }
-    //  public string GetEnd(){
-    //     return _endMessage;
-    // }
-    // public void SetEnd(string end) {
-    //     _endMessage = end;
-    // }
-    // public int GetDuration(){
-    //     return _duration;
-    // }
-    // public void SetDuration(int duration) {
-    //     _duration = duration;
-    // }
     public void GetReady() {
         
         List<string> animations = new List<string>();
@@ -113,12 +165,12 @@ class Activity {
         return summary;
     }
 
-    public void displayEnd(int duration) {
+    public void displayEnd(int duration, string name) {
         // how can I make the duration be set by the user input? 
         Console.WriteLine("");
         Console.WriteLine(_endMessage);
         GetReady();
-        Console.WriteLine($"\nYou have completed {duration} seconds of Breathing Activity");
+        Console.WriteLine($"\nYou have completed {duration} seconds of {name} Activity");
         GetReady();
     }
 }
@@ -136,7 +188,7 @@ class Breathing: Activity {
     }
 
     public void DisplayBreathIn() {
-        // how to display it on the same line?
+       
         Console.Write($"Breathe in...");
         for (int i = _breathIn; i > 0; i-- ) {
             
@@ -159,15 +211,33 @@ class Breathing: Activity {
 
 class Listening: Activity {
     private List<string> _prompts = new List<string>();
+    private List<string> entries = new List<string>();
 
     public Listening(string end, string description, int duration): base(end,description, duration){
+        _name = "Welcome to the Listening Activity";
         _prompts.Add("Who are people that you appreciate?");
         _prompts.Add("What are personal strengths of yours?");
         _prompts.Add("Who are people that you have helped this week?");
         _prompts.Add("When have you felt the Holy Ghost this month?");
         _prompts.Add("Who are some of your personal heroes?");
     }
-
+    public void Prompts() {
+        Random random = new Random();
+        Console.WriteLine("Consider the following prompt:\n");
+        int promptMessage = random.Next(_prompts.Count);
+        string prompts_ = _prompts[promptMessage];
+        Console.WriteLine($"--- {prompts_} ---\n");
+    }
+    public void userEntries() {
+        Console.Write(">");
+        string userInput = Console.ReadLine();
+        entries.Add(userInput);
+        
+    }
+    public void AllEntries() {
+        int count = entries.Count;
+        Console.WriteLine("You listed " + count + " items!");
+    }
     
 }
 
@@ -177,6 +247,7 @@ class Reflection: Activity {
 
 
     public Reflection(string end, string description, int duration): base(end, description , duration) {
+        _name = "Welcome to the Reflection Activity";
         _prompts.Add("Think of a time when you stood up for someone else.");
         _prompts.Add("Think of a time when you did something really difficult.");
         _prompts.Add("Think of a time when you helped someone in need.");
@@ -192,5 +263,30 @@ class Reflection: Activity {
         _questions.Add("How can you keep this experience in mind in the future?");
         }
 
-    
+    public void FirstPrompt() {
+        Random random = new Random();
+        Console.WriteLine("Consider the following prompt:\n");
+        int promptMessage = random.Next(_prompts.Count);
+        string prompts_ = _prompts[promptMessage];
+        Console.WriteLine($"--- {prompts_} ---\n");
+        Console.WriteLine("When you have something in mind, press enter to continue.");
+        Console.ReadLine();
+        Console.WriteLine("Now ponder in each of the following questions as they related to this experience.");
+        Console.Write($"You may begin in: ");
+        for (int i = 5; i > 0; i-- ) {
+            
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        } 
+    }
+
+    public void DisplayQuestion() {
+        Random random = new Random();
+        int message = random.Next(_questions.Count);
+        string questions_ = _questions[message];
+        Console.Write($"> {questions_}");
+        GetReady();
+        
+    }
 }
