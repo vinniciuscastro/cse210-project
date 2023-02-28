c91a46684bccdd355a9d3ab4c8f511be5232eb7d
sibervi <vinnicius.castro97@gmail.com>
2023-02-28 14:49:52 -0700

    something

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -7,3 +7,44 @@ class Program
         Console.WriteLine("Hello Learning05 World!");
     }
 }
+class Shape {
+    protected string _color;
+
+
+    public string getColor() {
+        return _color;
+    }
+
+    public void setColor(string color) {
+        _color = color;
+    }
+
+    // public double GetArea() {
+    //     return  
+    // }
+}
+
+class Square: Shape {
+    private double _side;
+
+    // public double GetArea() {
+    //     return  
+    // }
+}
+
+class Rectangle:Shape {
+    private double _length; 
+    private double _width; 
+
+    // public double GetArea() {
+    //     return  
+    // }
+}
+
+class Circle: Shape {
+    private double _radius; 
+
+    // public double GetArea() {
+    //     return  
+    // }
+}
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -101,3 +101,74 @@ using System;
 // class Student: Person {
 
 // }
+// Method Overriding 
+// When I child class provides its own implementation of a method
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         var employees = new List<Employee>();
+//         var salary = new Employee();
+//         var hourly = new HourlyEmployee();
+//         var exec = new Executive();
+
+//         employees.Add(salary);
+//         employees.Add(hourly);
+//         employees.Add(exec);
+//         foreach (var e in employees) {
+//             Console.WriteLine(e.CalculateYearPlay());
+//         }
+//         Console.WriteLine(salary.CalculateYearPlay());
+//         Console.WriteLine(hourly.CalculateYearPlay());
+//     }
+// }
+// class Employee {
+//     protected int Salary = 100000;
+
+//     virtual public int CalculateYearPlay() {
+//         return Salary;
+//     }
+// }
+// class Executive: Employee {
+//     private int bonus= 1000000;
+
+//     public override int CalculateYearPlay()
+//     {
+//         return Salary + bonus; 
+//     }
+// }
+// class HourlyEmployee: Employee {
+//     private int HourlyWage = 25;
+//     private int HoursPerYear = 2000;
+
+//     public override int CalculateYearPlay()
+//     {
+//         return HourlyWage * HoursPerYear;
+//     }
+// }
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+    }
+}
+
+abstract class Person {
+    abstract public string Speak(); 
+}
+
+class NicePerson: Person {
+    public override string Speak()
+    {
+        return "Hello! How are you today?";
+    }
+}
+class MeanPerson: Person {
+    public override string Speak()
+    {
+        return "Your code is the worst.";
+    }
+}
