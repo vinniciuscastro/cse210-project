34d2854f2ddec119451af1d6dad5eed690277310
sibervi <vinnicius.castro97@gmail.com>
2023-03-09 15:13:23 -0700

    class work

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -5,9 +5,12 @@ class Program
     static void Main(string[] args)
     {
 
-
+        List<Goal> _allGoals = new List<Goal>(); 
+        int total_points = 0;
         bool loop = true;
         while (loop){
+            Console.WriteLine($"You have {total_points} points.");
+            Console.WriteLine("");
             Console.WriteLine("Menu Options:");
             Console.WriteLine("1. Create a New Goal");
             Console.WriteLine("2. List Goals");
@@ -19,6 +22,63 @@ class Program
             int prompt = int.Parse(Console.ReadLine() ??"");
             if (prompt == 6) {
                 loop = false;  
+            }
+            else if (prompt == 1) {
+                Console.WriteLine("The types of Goals are:");
+                Console.WriteLine("1. Simple Goal");
+                Console.WriteLine("2. Eternal Goal");
+                Console.WriteLine("3. Checklist Goal");
+                Console.Write("Which type of goal would you like to create? ");
+                int answer = int.Parse(Console.ReadLine() ??"");
+                if (answer == 1) {
+                    Console.Write("What is the name of your goal? ");
+                    string goal = Console.ReadLine() ??"";
+                    Console.Write("What is a short description of it? ");
+                    string describe  = Console.ReadLine() ??"";
+                    Console.Write("What is the amount of points associated with this goal? ");
+                    int points = int.Parse(Console.ReadLine() ??"");
+                    Simple simple = new Simple(points, goal, describe);
+                    _allGoals.Add(simple);
+                    
+
+
+                }
+                else if (prompt == 2) {
+                    Console.Write("What is the name of your goal? ");
+                    string goal = Console.ReadLine() ??"";
+                    Console.Write("What is a short description of it? ");
+                    string describe  = Console.ReadLine() ??"";
+                    Console.Write("What is the amount of points associated with this goal? ");
+                    int points = int.Parse(Console.ReadLine() ??"");
+                    Eternal eternal = new Eternal(points, goal, describe);
+                    _allGoals.Add(eternal);
+                }
+                else if (prompt == 3) {
+                    Console.Write("What is the name of your goal? ");
+                    string goal = Console.ReadLine() ??"";
+                    Console.Write("What is a short description of it? ");
+                    string describe  = Console.ReadLine() ??"";
+                    Console.Write("What is the amount of points associated with this goal? ");
+                    int points = int.Parse(Console.ReadLine() ??"");
+                    Console.Write("How many times does this goal need to be accomplished for a bonus? ");
+                    int quantity = int.Parse(Console.ReadLine() ??"");
+                    Console.Write("How many times does this goal need to be accomplished for a bonus? ");
+                    int bonus = int.Parse(Console.ReadLine() ??"");
+                    Checklist checklist = new Checklist(points, goal, describe, quantity, bonus);
+                    _allGoals.Add(checklist);
+                }
+            }
+            else if (prompt == 2) {
+                
+            }
+            else if (prompt == 3) {
+                
+            }
+            else if (prompt == 4) {
+                
+            }
+            else if (prompt == 5) {
+                
             }
         }
        
@@ -55,28 +115,23 @@ class Program
             
 
         }
-
+    
     }
 }
 class Goal {
     protected int _points; 
     protected string _goal; 
     protected  string _description;
-    protected List<String> _allGoal = new List<string>(); 
     protected bool _complete = false; 
 
-    public Goal (int points, string goal, string description, bool complete) {
+    public Goal (int points, string goal, string description) {
         _points = points; 
         _description = description; 
-        _complete = complete; 
+        
 
     }
-    virtual public void GetGoalFromUser() {
-
-    }
-
     virtual public void Display() {
-
+        
     }
     virtual public void RecordEvent() {
 
@@ -90,20 +145,34 @@ class Goal {
 
 class Simple: Goal {
 
-    public Simple(int points, string goal, string description, bool complete):base (points, goal, description, complete) {
-
+    public Simple(int points, string goal, string description):base (points, goal, description) {
 }
-}
+    public override void Display()
+    {
+        Console.WriteLine($"[{_complete}] {_goal} ({_description})");
+    }
+} 
+    
 class Eternal: Goal {
-    public Eternal(int points, string goal, string description, bool complete):base (points, goal, description, complete) {
+    public Eternal(int points, string goal, string description):base (points, goal, description) {
 
 }
+    public override void Display()
+    {
+        Console.WriteLine($"[{_complete}] {_goal} ({_description})");
+    }
 
 }
 
 class Checklist: Goal { 
-    public Checklist(int points, string goal, string description, bool complete):base (points, goal, description, complete) {
-
+    private int _quantity; 
+    private int _bonus;
+    public Checklist(int points, string goal, string description, int quantity, int bonus):base (points, goal, description) {
+        _quantity = quantity; 
+        _bonus = bonus; 
 }
-
+    public override void Display()
+    {
+        Console.WriteLine($"[{_complete}] {_goal} ({_description}) ---Currently completed: {_quantity}");
+    }
 }
