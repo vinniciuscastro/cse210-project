42feb7c2c511d61002397d3bf55fdea1a75b19ec
sibervi <vinnicius.castro97@gmail.com>
2023-03-23 14:55:06 -0600

    1 done

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,18 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Video video = new Video();
-        Comment comment = new Comment();
+        Video video = new Video("How to be the best person:", "Vinnicius Castro", 2, 10);
+        Comment comment = new Comment("Zendaya", "SERVING SERVING SERVING, you are the best love ya");
+        Comment comment1 = new Comment("Miley Cyrus", "I loved your video thank you for being so thoughtful");
+        Comment comment2 = new Comment("Adele", "This video is so monumental!");
+        Video video1 = new Video("Don't be a hater", "Vinni", 0,08);
+        Comment comment3 = new Comment("Taylor Swift", "Haters are gonna hate");
+        Comment comment4 = new Comment("Beyonce", "Respect that and bowl down");
+        Comment comment5 = new Comment("Rihanna", "Like bra, bra, bra");
+        Video video3 = new Video("How to be polite", "VC", 1, 42);
+        Comment comment6 = new Comment("Selena Gomez", "I love you like a love song baby");
+        Comment comment7 = new Comment("Drake", "It go, right foot up, left foot slide");
+        Comment comment8 = new Comment("Pikachu", "Pika Pika chuuu");
 
-        Console.WriteLine("Video information:");
-        Console.WriteLine(video.Display("How to be the best person:", "Vinnicius Castro", 2, 10));
+
+
+        Console.WriteLine("Video 1 - information:");
+        Console.WriteLine(video.Display());
         Console.WriteLine();
         Console.WriteLine("Comments:");
-        Console.WriteLine(comment.Display("Zendaya", "SERVING SERVING SERVING, you are the best love ya"));
+        Console.WriteLine(comment.Display());
         Console.WriteLine("-------------------------------------------------------------");
-        Console.WriteLine(comment.Display("Miley Cyrus", "I loved your video thank you for being so thoughtful"));
+        Console.WriteLine(comment1.Display());
         Console.WriteLine("-------------------------------------------------------------");
-        Console.WriteLine(comment.Display("Adele", "This video is so monumental!"));
+        Console.WriteLine(comment2.Display());
+        Console.WriteLine();
+        Console.WriteLine("Video 2 - information:");
+        Console.WriteLine(video1.Display());
+        Console.WriteLine();
+        Console.WriteLine("Comments:");
+        Console.WriteLine(comment3.Display());
+        Console.WriteLine("-------------------------------------------------------------");
+        Console.WriteLine(comment4.Display());
+        Console.WriteLine("-------------------------------------------------------------");
+        Console.WriteLine(comment5.Display());
+        Console.WriteLine();
+        Console.WriteLine("Video 3 - information:");
+        Console.WriteLine(video3.Display());
+        Console.WriteLine();
+        Console.WriteLine("Comments:");
+        Console.WriteLine(comment6.Display());
+        Console.WriteLine("-------------------------------------------------------------");
+        Console.WriteLine(comment7.Display());
+        Console.WriteLine("-------------------------------------------------------------");
+        Console.WriteLine(comment8.Display());
+        Console.WriteLine();
+        
         
     }
 }
@@ -26,7 +60,14 @@ class Video {
     public int min;
     public int sec;
 
-    public string Display(string title, string author, int min, int sec){ 
+    public Video(string title1, string author1, int min1, int sec1) {
+        min = min1;
+        sec = sec1;
+        author = author1;
+        title = title1;
+
+    }
+    public string Display(){ 
         return $"Video Title: {title} Author: {author}\nThe total length of the video is: {min}:{sec}";
     }
 }
@@ -35,7 +76,11 @@ class Comment {
     public string name;
     public string comment;
 
-    public string Display(string name, string comment) {
+    public Comment(string name1, string comment2){
+        name = name1;
+        comment = comment2;
+    }   
+    public string Display() {
         return $"  Name: {name}\n  {comment}";
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -7,3 +7,38 @@ class Program
         Console.WriteLine("Hello Foundation2 World!");
     }
 }
+
+
+class Product {
+    private string _product;
+    private int _id;
+    private int _price;
+    private int _quantity;
+
+    public Product(string product, int id, int price, int quantity) {
+        _product = product;
+        _id = id;
+        _price = price;
+        _quantity = quantity;
+    } 
+    
+}
+
+class Address {
+    private string _city;
+    private string _state;
+    private string _province;
+    private string _country;
+    private int _shipping; 
+
+}
+
+class Customer {
+    private string _name;
+    Address address = new Address();
+}
+class Order {
+    Product p = new Product();
+    Customer c = new Customer();
+
+}
