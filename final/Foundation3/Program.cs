92c8c6a100c5cfb7b2e51f23618066b2609a672f
sibervi <vinnicius.castro97@gmail.com>
2023-03-29 16:50:29 -0600

    done

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,26 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Console.WriteLine("--------------------------------------------------------------------------");
+        Address address = new Address("Rexburg", "Idaho", "STC - 367", "USA");
+        Lecture lecture = new Lecture("How to be a nice person", "To be a nice person you need to do nice things", "2:00 pm", "April 1st 2023", address , "Brother Moseley", 30);
+        lecture.DisplayLecture();
+        Console.WriteLine();
+        Console.WriteLine("--------------------------------------------------------------------------");
+        Address address1 = new Address("New York", "New York", "Met Museum", "USA");
+        Reception reception = new Reception("Met gala", "Red carpet with worldwide super stars", "6:00 pm", "May 2nd", address1, "Welcome to the Met Gala we are happy to have you here", "Yes");
+        reception.DisplayReception();
+        Console.WriteLine();
+        Console.WriteLine("--------------------------------------------------------------------------");
+        Address address2 = new Address("London", "London", "Hampstead Heath", "United Kingdom");
+        Outdoor outdoor = new Outdoor("Adele concert", "The singer adele will be performing live", "3:00 pm", "July 6th 2023", address2,"Sunshine");
+        outdoor.DisplayOutdoor();
     }
 }
 
 class Address {
     private string _city;
     private string _state;
-    private string _province;
+    private string _location;
     private string _country;
 
-    public Address(string city, string state, string province, string country) {
+    public Address(string city, string state, string location, string country) {
         _city = city;
         _state = state;
-        _province = province;
+        _location = location;
         _country = country;
-    
     }
 
     public string DisplayAddress() {
-        return $"Event address\nCity: {_city} State: {_state} Province: {_province} Country: {_country}";
+        return $"Event address\nCity: {_city} State: {_state} Country: {_country} Location: {_location}";
     }
 
 }
@@ -39,6 +51,7 @@ class Event {
         _title = title;
         _description = description;
         _time = time; 
+        _address = address;
     }
 
 }
@@ -53,7 +66,8 @@ class Lecture:Event {
         _capacity = capacity;
     }
     public void DisplayLecture() {
-        Console.WriteLine($"Lecture Title: {_title}");
+        Console.WriteLine($"Short Description - Lecture Title: {_title}\n> {_description}\n> Date: {_date} Time: {_time}\n> Address: {_address.DisplayAddress()}");
+        Console.WriteLine($"\nFull Description - Lecture by {_speaker} titled {_title}\n{_description}\n> Date: {_date} Time: {_time}\n> Location Capacity: {_capacity}\n> Address: {_address.DisplayAddress()}");
     }
 }
 
@@ -66,7 +80,8 @@ class Reception:Event{
         _register = register;
     }
     public void DisplayReception(){
-
+        Console.WriteLine($"Short Description - Reception Title: {_title}\n> {_description}\n> Date: {_date} Time: {_time}\n> Address {_address.DisplayAddress()}");
+        Console.WriteLine($"\nFull Description - Reception titled {_title}\n{_description}\n> Registration: {_register}\n> Date: {_date} Time: {_time}\n> Address: {_address.DisplayAddress()}\n>RSVP: {_rsvp}");
     }
 
 }
@@ -80,7 +95,8 @@ class Outdoor:Event {
     }
 
     public void DisplayOutdoor(){
-
+        Console.WriteLine($"Short Description - Outdoor Title: {_title}\n> {_description}\n> Date: {_date} Time: {_time}\n> Address {_address.DisplayAddress()}");
+        Console.WriteLine($"\nFull Description - Outdoor event titled {_title}\n{_description}\n> Date: {_date} Time: {_time}\n> Address: {_address.DisplayAddress()}\n>Weather: {_weather}");
     }
 
 }
